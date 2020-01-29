63e99713b0ca25cea8a64fc1d0d98021cbe4adef
hortenssiaa@gmail.com <hortenssiaa@gmail.com>
2020-01-30 01:29:27 +0900

    test

---
+++ GithubTest/app/src/main/java/com/example/githubtest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.githubtest;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button1);
+    }
+}
