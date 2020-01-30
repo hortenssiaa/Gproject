ae33beff3a18a4829cb898b3a0ce70aed5382b6c
hortenssiaa@gmail.com <hortenssiaa@gmail.com>
2020-01-30 19:00:58 +0900

    Edited MainActivity.java

--- ImplicitIntent/app/src/main/java/com/example/implicitintent/MainActivity.java
+++ ImplicitIntent/app/src/main/java/com/example/implicitintent/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.implicitintent;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View view){
+        Intent intent = null;
+
+        switch (((Button)view).getId()){
+            case R.id.phonecall:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:+82123456789"));
+                break;
+
+            case R.id.maps:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:37.30.127.2?z=10"));
+                break;
+
+            case R.id.web:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com"));
+                break;
+
+            case R.id.contacts:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://contacts/people/"));
+                break;
+        }
+
+        if(intent != null)
+            startActivity(intent);
+    }
+
 }
