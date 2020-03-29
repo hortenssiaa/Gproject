4092598007ccd733e0283c9578c374f28429d0e5
hortenssiaa@gmail.com <hortenssiaa@gmail.com>
2020-03-29 18:45:43 +0900

    first commit

--- Bss_Gproject/app/src/main/java/com/example/bss_gproject/MainActivity.java
+++ Bss_Gproject/app/src/main/java/com/example/bss_gproject/MainActivity.java
@@ -6,7 +6,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    wefewefewawefwefew
+    /*
+    MapView mapView = new MapView(this);
+    ct_map.addView(mapView);
+     */
+/*
+    try {
+        PackageInfo info = getPackageManager().getPackageInfo("com.yjw.android.busanbus",
+                PackageManager.GET_SIGNATURES);
+        for (Signature signature : info.signatures) {
+            MessageDigest md = MessageDigest.getInstance("SHA");
+            md.update(signature.toByteArray());
+            String str = Base64.encodeToS tring(md.digest(), Base64.DEFAULT);
+            Log.d("KeyHash:", str);
+            Toast.makeText(this, str, Toast.LENGTH_LONG).show();
+        }
+    } catch(NoSuchAlgorithmException e){
+        e.printStackTrace();
+    } catch(PackageManager.NameNotFoundException e){
+        e.printStackTrace();
+    }
+*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
